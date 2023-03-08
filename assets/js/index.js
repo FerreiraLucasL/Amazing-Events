731f0d99440758640f606cad0835e5e8ee365c63
Ferreira Lucas <lukasm4a1@gmail.com>
2023-03-08 09:14:22 -0300

    checkpoint

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,8 +3,10 @@ import data from './data.js'
 let events = data.events
 const cards = document.getElementById("cards");
 
+
 //imprimir cartas
-function createCards(events,cards) {
+function createCards(events, cards) {
+    cards.innerHTML = ""
     for(let event of events) {
         let div = document.createElement("div");
         div.className = "card";
@@ -15,11 +17,73 @@ function createCards(events,cards) {
                 <p class="card-text">${event.place}</p>
                 <p class="card-text">${event.date}</p>
                 <p class="card-text">${event.category}</p>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal">Details</button>
             </div>
         `
         cards.appendChild(div)                
     }    
 }
-createCards(events,cards)
+createCards(events, cards)
+
+//mapeo categorias (con repetidos)
+const mapCategories = (array) => {
+    return array.map(categories => categories.category.toLowerCase())    
+}
+let allCategories = mapCategories(events)
+
+//filtro repetidos categorias
+let categories
+const eliminarRepetidos = (array) => {        
+    let arrayDevuelto
+    arrayDevuelto = array.reduce((acc, elem) => {
+        if(!acc.includes(elem)){
+            acc.push(elem)
+        }
+        return acc
+    }, [])
+    return arrayDevuelto
+}
+categories = eliminarRepetidos(allCategories)
+
+//checkbocks categorias
+const catsChecks = document.getElementById("categories");
+const createCatsChecks = (array, catsChecks) => {
+    array.forEach(category =>{
+        let div = document.createElement("div")
+        div.className = `checksCategory ${category}`
+        div.innerHTML = `
+        <input type="checkbox" id=${category} name="category"/>
+        <label for="${category}">${category}</label>
+        `        
+        catsChecks.appendChild(div)
+    })
+}
+//categories.push("todos")
+createCatsChecks(categories, catsChecks)
+
+//filtrado por categorias
+const filterChecks = (array) => {
+    let checked = document.querySelector('input[type=checkbox]:checked');    
+    let filteredArray = array.filter(element => element.category.toLowerCase().includes(checked.id))
+    return filteredArray
+}
+
+catsChecks.addEventListener('change', () =>{
+    let catsFilter = filterChecks(events)
+    createCards(catsFilter, cards)
+})
+
+//busqueda por nombre
+const filterSearch = (array, value) => {
+    let filteredArray = array.filter(element => (element.name.toLowerCase().includes(value.toLowerCase())))
+    return filteredArray
+}
+
+const $search = document.getElementById("search")
+$search.addEventListener('keyup', (e) => {
+    let eventFilter = filterSearch(events, e.target.value)
+    console.log(e.target.value)
+    createCards(eventFilter, cards)
+})
+
 
